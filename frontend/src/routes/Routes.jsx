2503dfc756940b549632ddb938f73281bcc4e013
anup kushwaha <anupk6347@gmail.com>
2025-03-01 08:52:35 +0530

    signup

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../components/Home';
 import Login from '../components/Login';
-import Signup from '../components/Signup';
+import Signup from '../components/SignUp';
 import Browse from '@/components/Browse';
 import Jobs from '@/components/Jobs';
 import Profile from '@/components/Profile';
